51d3a59d57e44fb266a6ce78f687f564eb47c420
Doston <nnuraliyev51@gmail.com>
2026-01-09 10:56:49 +0500

    navbarrni  yangiladim va rasimni ozgartirdim

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,13 +4,16 @@ import { useSelector } from 'react-redux'
 import { useEffect, useState } from 'react'
 
 function Navbar() {
-  const cartProducts = useSelector(state => state.products)
+  const cartProducts = useSelector(state => state.products || [])
   const [scrolled, setScrolled] = useState(false)
 
   useEffect(() => {
-    const onScroll = () => setScrolled(window.scrollY > 20)
-    window.addEventListener('scroll', onScroll)
-    return () => window.removeEventListener('scroll', onScroll)
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 20)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
   }, [])
 
   return (
@@ -30,7 +33,7 @@ function Navbar() {
             }
           `}
         >
-      
+
           <Link
             to="/"
             className="text-[24px] font-black tracking-tight text-amber-900"
@@ -38,48 +41,29 @@ function Navbar() {
             Bozorcha<span className="text-orange-500">.uz</span>
           </Link>
 
-          <Link
-            to="/cart"
-            className="ml-auto relative group focus:outline-none"
-
-            {!!cartProducts?.length && (
-              <span
-                className="
-                  absolute
-                  -top-1.5
-                  -right-1.5
-                  z-10
-                  min-w-[20px]
-                  h-[20px]
-                  px-1
-                  rounded-full
-                  bg-red-500
-                  text-white
-                  text-[11px]
-                  font-bold
-                  flex items-center justify-center
-                  ring-2 ring-white
-                  shadow
-                "
-              >
+          <Link to="/cart" className="ml-auto relative group">
+            {cartProducts.length > 0 && (
+              <span className="
+                absolute -top-1.5 -right-1.5 z-10
+                min-w-[20px] h-[20px] px-1
+                rounded-full bg-red-500 text-white
+                text-[11px] font-bold
+                flex items-center justify-center
+                ring-2 ring-white shadow
+              ">
                 {cartProducts.length > 9 ? '9+' : cartProducts.length}
               </span>
             )}
 
-            <div
-              className="
-                w-11 h-11
-                rounded-xl
-                bg-amber-600
-                text-white
-                flex items-center justify-center
-                shadow-md
-                transition-all duration-200
-                group-hover:bg-amber-700
-                group-hover:scale-105
-                active:scale-95
-              "
-            >
+            <div className="
+              w-11 h-11 rounded-xl
+              bg-amber-600 text-white
+              flex items-center justify-center
+              shadow-md transition-all duration-200
+              group-hover:bg-amber-700
+              group-hover:scale-105
+              active:scale-95
+            ">
               <RiShoppingCartFill size={20} />
             </div>
           </Link>
